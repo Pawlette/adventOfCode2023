bdb839747f16ecade5a9c8ef90de16b33d67d45f
Paulette Amparo <pauletteazb@gmail.com>
2023-12-10 16:31:51 -0800

    Solution part 2 day 3

--- day3.py
+++ day3.py
@@ -14,6 +14,60 @@ def checkUpDown(matrix, i, j):
 
     return False
 
+def getCellMult(matrix, i, j, asterisks):
+    key = ""
+    foundAsterisks = set()
+    if matrix[i][j] == "*":
+        key = str(i) + "," + str(j)
+        foundAsterisks.add(key)
+
+    if i > 0 and matrix[i-1][j] == "*":
+        key = str(i-1) + "," + str(j)
+        foundAsterisks.add(key)
+
+    if i < len(matrix)-1 and matrix[i+1][j] == "*":
+        key = str(i+1) + "," + str(j)
+        foundAsterisks.add(key)
+
+    if key != "" and key not in asterisks:
+        asterisks[key] = []
+
+    return foundAsterisks
+
+def getPartNumbersMulti(matrix):
+    sum = 0
+    asterisks = {}
+    numCols = len(matrix[0])-1
+    for row in range(len(matrix)):
+        foundAsterisks = set()
+        strNum = ""
+        # iterate through columns
+        for column in range(len(matrix[0])):
+            val = matrix[row][column]
+            # print("val:", val)
+            asterisksCurrent = getCellMult(matrix, row, column, asterisks)
+            foundAsterisks.update(asterisksCurrent)
+            # print("row:", row, "column:", column, "asterisksCurrent:", asterisksCurrent, " foundAsterisks:", foundAsterisks, "asterisks:", asterisks)
+
+            if val.isdigit():
+                strNum += val
+            if not val.isdigit() or column == numCols:
+                newNum = getValue(strNum)
+                strNum = "" # Reset string
+                if newNum > 0:
+                    for key in foundAsterisks:
+                        if key in asterisks:
+                            for num in asterisks[key]:
+                                # print('num:{0} * newNum:{1}'.format(num, newNum))
+                                sum += num * newNum
+                            asterisks[key].append(newNum)
+                        else:
+                            asterisks[key] = [newNum]
+
+                foundAsterisks = asterisksCurrent
+    return sum
+
+
 def getPartNumbersSum(matrix):
     sum = 0
     # iterate through rows
@@ -68,7 +122,7 @@ def getSum(file):
     # print('matrix: {0}'.format(matrix))
 
     print('1: {0}'.format(getPartNumbersSum(matrix)))
-    #return sum
+    print('2: {0}'.format(getPartNumbersMulti(matrix)))
 
 input = open("inputDay3.txt", "r") 
 getSum(input)
